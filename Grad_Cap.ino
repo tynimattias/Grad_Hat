4ee00052b7a141f731a9fa2db25087d0c2b2a177
tynimattias <72426180+tynimattias@users.noreply.github.com>
2022-06-21 11:12:05 -0700

    Add Commented Description

--- Grad_Cap.ino
+++ Grad_Cap.ino
@@ -2,17 +2,12 @@
 
 #define DELAY 5000
 
+//Code for the Arduino Mega controlling the LED Matrix,
+//Each image is about 1024 lines of code since each pixel is individually drawn
+//This code was written using the Python script img_to_code.py
 
-// Most of the signal pins are configurable, but the CLK pin has some
-// special constraints.  On 8-bit AVR boards it must be on PORTB...
-// Pin 11 works on the Arduino Mega.  On 32-bit SAMD boards it must be
-// on the same PORT as the RGB data pins (D2-D7)...
-// Pin 8 works on the Adafruit Metro M0 or Arduino Zero,
-// Pin A4 works on the Adafruit Metro M4 (if using the Adafruit RGB
-// Matrix Shield, cut trace between CLK pads and run a wire to A4).
 
-//#define CLK  8   // USE THIS ON ADAFRUIT METRO M0, etc.
-//#define CLK A4 // USE THIS ON METRO M4 (not M0)
+
 #define CLK 11 // USE THIS ON ARDUINO MEGA
 #define OE   9
 #define LAT 10
